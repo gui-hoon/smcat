c01c36cbd04e0981816129a03725ce458e2c6c36
gui-hoon <81810766+gui-hoon@users.noreply.github.com>
2022-01-23 23:27:17 +0900

    Delete cctv directory

--- cctv/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-# app_name = cctv
-urlpatterns = [
-    path('', views.mycctv, name='mycctv'),
-    path('mycam/', views.mycam, name='mycam')
-]
